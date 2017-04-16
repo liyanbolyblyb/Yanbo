ebe2682cff28f9a7de13d3ee0a139c641acf417d
yanboli <yanboli@172.16.14.39>
2017-04-16 12:17:41 -0400

    figure

--- BusyBugs/src/project_4/Busy.java
+++ BusyBugs/src/project_4/Busy.java
@@ -1,5 +1,42 @@
 package project_4;
 
+class map{
+	private int creature[];
+	private boolean flag[];
+	private int suitPieces[];
+	
+	public void setCreature(int c[]){
+		this.creature = new int[9];
+		for(int i = 0; i < 9; i++)
+		{
+			creature[i] = c[i];
+		}
+	}
+	
+	public void setFlag(){
+		this.flag = new boolean [9];
+		for(int i = 0; i < 9; i++)
+		{
+			flag[i] = true;
+		}
+	}
+	
+	public boolean[] getFlag(){
+		return this.flag;
+	}
+
+	public void setPieces() {
+		this.suitPieces = new int [4];
+		for(int i = 0; i < 4; i++)
+		{
+			suitPieces[i] = i+1;
+		}
+	}
+	
+	
+}
+
 public class Busy {
-int a = 1;
+	map[] m = new map[4];
+
 }
